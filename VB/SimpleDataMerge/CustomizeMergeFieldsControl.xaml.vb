8cd3a886bed005f4990b9ca626023e00f60118ae
DevExpressExampleBot <fakeEmail>
2021-09-03 15:59:30 +0300

    Source auto update [skip ci]

--- VB/SimpleDataMerge/CustomizeMergeFieldsControl.xaml.vb
+++ VB/SimpleDataMerge/CustomizeMergeFieldsControl.xaml.vb
@@ -6,15 +6,15 @@ Imports System.Windows
 Imports System.Windows.Controls
 
 Namespace SimpleDataMerge
-    ''' <summary>
-    ''' Interaction logic for CustomizeMergeFieldsControl.xaml
-    ''' </summary>
-    Partial Public Class CustomizeMergeFieldsControl
-        Inherits UserControl
+	''' <summary>
+	''' Interaction logic for CustomizeMergeFieldsControl.xaml
+	''' </summary>
+	Partial Public Class CustomizeMergeFieldsControl
+		Inherits UserControl
 
-        Public Sub New(ByVal mergeFieldsNames() As MergeFieldNameInfo)
-            InitializeComponent()
-            grid.ItemsSource = mergeFieldsNames
-        End Sub
-    End Class
+		Public Sub New(ByVal mergeFieldsNames() As MergeFieldNameInfo)
+			InitializeComponent()
+			grid.ItemsSource = mergeFieldsNames
+		End Sub
+	End Class
 End Namespace
